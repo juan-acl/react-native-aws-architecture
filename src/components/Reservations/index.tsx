862ae3f2a0cbbc046c0a75cbfd4cf9f978280bf6
unknown <dev.juan.eloper@gmail.com>
2024-06-28 20:38:34 -0600

    Reservations component

--- src/components/Reservations/index.tsx
+++ src/components/Reservations/index.tsx
@@ -1,9 +1,50 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface Reservation {
+  id: number;
+  idHotel: number;
+  nameHotel: string;
+  address: string;
+  phone: string;
+  daysReservations: number;
+  date: Date;
+}
 
 export const Reservations = () => {
+  const [Reservations, setReservations] = useState<Reservation[]>([]);
   return (
-    <View>
-      <Text>Reservations</Text>
-    </View>
+    <>
+      {
+        Reservations.length === 0
+          ?
+          <View style={styles.containerWithoutReservation} >
+            <Text> Ups! No tienes reservaciones agregadas</Text>
+          </View>
+          :
+          <>
+            <View>
+              <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+                {
+                  Reservations.map((reservation: Reservation, index: number) => {
+                    return (
+                      <></>
+                    )
+                  })
+                }
+              </View>
+            </View>
+          </>
+
+      }
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  containerWithoutReservation: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
