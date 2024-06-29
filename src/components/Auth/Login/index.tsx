7fbd0b0cef8b5fbfaa288524b571fa58ea484692
unknown <dev.juan.eloper@gmail.com>
2024-06-28 22:03:01 -0600

    Fix signIn amplify --Unknown error has ocurred

--- src/components/Auth/Login/index.tsx
+++ src/components/Auth/Login/index.tsx
@@ -1,40 +1,43 @@
-import React from "react";
-import {View, TextInput, Button} from "react-native";
-import { signIn, SignInInput } from 'aws-amplify/auth';
-import { Authenticator } from '@aws-amplify/ui-react-native';
+import React, { useState } from "react";
+import { View, TextInput, Button } from "react-native";
+import { signIn, SignInInput, signOut } from 'aws-amplify/auth';
 
 export const Login = () => {
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+    });
 
     const login = async () => {
         try {
-            const {username, password} = {
-                username: "juan@correo.com",
-                password: "juan12345",
+            const { username, password }: SignInInput = {
+                username: data.email,
+                password: data.password,
             };
-            const {isSignedIn, nextStep} = await signIn({username, password});
-            console.log(isSignedIn, nextStep);
+            const { isSignedIn, nextStep } = await signIn({
+                username,
+                password,
+                options: {
+                    authFlowType: 'USER_PASSWORD_AUTH'
+                }
+            });
+            console.log({ isSignedIn, nextStep });
         } catch (e) {
             console.log("error signing in", e);
         }
     };
 
-    const services = {
-        async handleSignIn(input: SignInInput) {
-            try {
-                const user = await signIn(input);
-                console.log(user);
-                return user;
-            } catch (error) {
-                console.error(error)
-            }
+    const logOut = async () => {
+        try {
+            await signOut({ global: true });
 
-        },
-    };
+        } catch (e) {
+            console.log("error signing out: ", e);
+        }
+    }
 
     return (
-        <View style={{padding: 20, marginTop: 20}}>
+        <View style={{ padding: 20, marginTop: 20 }}>
             <TextInput
                 style={{
                     height: 40,
@@ -43,8 +46,8 @@ export const Login = () => {
                     marginTop: 10,
                     padding: 5,
                 }}
-                onChangeText={(text) => setEmail(text)}
-                value={email}
+                onChangeText={(text) => setData({ ...data, email: text })}
+                value={data.email}
                 placeholder="Correo electrónico"
                 keyboardType="email-address"
                 autoCapitalize="none"
@@ -57,14 +60,20 @@ export const Login = () => {
                     marginTop: 10,
                     padding: 5,
                 }}
-                onChangeText={(text) => setPassword(text)}
-                value={password}
+                onChangeText={(text) => setData({ ...data, password: text })}
+                value={data.password}
                 placeholder="Contraseña"
                 secureTextEntry={true}
             />
             <Button
                 onPress={login}
                 title="Iniciar sesión"
+                color="#841584"
+                accessibilityLabel="Iniciar sesión"
+            />
+            <Button
+                onPress={logOut}
+                title="Cerrar sesión"
                 color="#841584"
                 accessibilityLabel="Iniciar sesión"
             />
