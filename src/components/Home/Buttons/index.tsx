17b30694a79f8abad6669024774dd798dfe114f1
unknown <dev.juan.eloper@gmail.com>
2024-07-01 11:55:08 -0600

    Format

--- src/components/Home/Buttons/index.tsx
+++ src/components/Home/Buttons/index.tsx
@@ -1,48 +1,48 @@
 import React from "react";
-import { View, Text, Pressable } from "react-native";
-import { styles } from "../styles";
-import { HomeProps } from "@/src/navigator/types/navigationStack";
-import { RoutesNameScreens } from "@/src/navigator/stack/nameScreens";
+import {View, Text, Pressable} from "react-native";
+import {styles} from "../styles";
+import {HomeProps} from "@/src/navigator/types/navigationStack";
+import {RoutesNameScreens} from "@/src/navigator/stack/nameScreens";
 
 const Buttons = (props: HomeProps) => {
-  const { navigation } = props;
+    const {navigation} = props;
 
-  const goToSignIn = () => {
-    navigation.navigate(RoutesNameScreens.SignIn);
-  };
+    const goToSignIn = () => {
+        navigation.navigate(RoutesNameScreens.SignIn);
+    };
 
-  const goToHotels = () => {
-    navigation.navigate(RoutesNameScreens.navigationTab);
-  };
+    const goToHotels = () => {
+        navigation.navigate(RoutesNameScreens.navigationTab);
+    };
 
-  const goToSignUp = () => {
-    navigation.navigate(RoutesNameScreens.SignUp);
-  };
+    const goToSignUp = () => {
+        navigation.navigate(RoutesNameScreens.SignUp);
+    };
 
-  return (
-    <>
-      <View style={styles.header}>
-        <Text style={styles.title}>MrHotel</Text>
-      </View>
-      <View style={styles.content}>
-        <Text style={styles.subtitle}>Reservaciones fáciles</Text>
-        <Text style={styles.summary}>
-          ¡Busca los mejores hoteles para pasar unas buenas vacaciones!
-        </Text>
-      </View>
-      <View style={styles.containerButtons}>
-        <Pressable style={styles.button2} onPress={goToSignUp}>
-          <Text style={styles.text1}>Regístrate</Text>
-        </Pressable>
-        <Pressable style={styles.button1} onPress={goToSignIn}>
-          <Text style={styles.text2}>Iniciar Sesión</Text>
-        </Pressable>
-      </View>
-      <Pressable style={styles.button1} onPress={goToHotels}>
-        <Text style={styles.text2}>Ver hoteles</Text>
-      </Pressable>
-    </>
-  );
+    return (
+        <>
+            <View style={styles.header}>
+                <Text style={styles.title}>MrHotel</Text>
+            </View>
+            <View style={styles.content}>
+                <Text style={styles.subtitle}>Reservaciones fáciles</Text>
+                <Text style={styles.summary}>
+                    ¡Busca los mejores hoteles para pasar unas buenas vacaciones!
+                </Text>
+            </View>
+            <View style={styles.containerButtons}>
+                <Pressable style={styles.button2} onPress={goToSignUp}>
+                    <Text style={styles.text1}>Regístrate</Text>
+                </Pressable>
+                <Pressable style={styles.button1} onPress={goToSignIn}>
+                    <Text style={styles.text2}>Iniciar Sesión</Text>
+                </Pressable>
+            </View>
+            <Pressable style={styles.button1} onPress={goToHotels}>
+                <Text style={styles.text2}>Ver hoteles</Text>
+            </Pressable>
+        </>
+    );
 };
 
 export default Buttons;
