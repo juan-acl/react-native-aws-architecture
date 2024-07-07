32030cada3d69829261de6c5a37c763d1f526d94
unknown <dev.juan.eloper@gmail.com>
2024-07-07 13:27:27 -0600

    Changes

--- src/components/Hotels/BottomSheet/index.tsx
+++ src/components/Hotels/BottomSheet/index.tsx
@@ -1,55 +1,42 @@
-import React, { useCallback, useMemo } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import React, { useCallback, useMemo } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
 interface Props {
-    bottomSheetRef: React.RefObject<BottomSheet>;
+  bottomSheetRef: React.RefObject<BottomSheet>;
 }
 
-export const ActionSheetHotel: React.FC<Props> = ({ bottomSheetRef }: Props) => {
-    // ref
+export const ActionSheetHotel: React.FC<Props> = ({
+  bottomSheetRef,
+}: Props) => {
+  const snapPoints = useMemo(() => [0.1, "50%", "90%"], []);
 
-    // snapPoints
-    const snapPoints = useMemo(() => [0.1, '50%', '90%'], []);
-
-    // callbacks
-    const handleSheetChanges = useCallback((index: number) => {
-        console.log('handleSheetChanges', index);
-    }, []);
-
-    // handle button press to open bottom sheet
-    const handleOpenPress = useCallback(() => {
-        bottomSheetRef.current?.expand();
-    }, []);
-
-    // renders
-    return (
-        <View style={styles.container}>
-            <BottomSheet
-                ref={bottomSheetRef}
-                snapPoints={snapPoints}
-                onChange={handleSheetChanges}
-                enablePanDownToClose={true}
-                index={1}  // initially closed
-            >
-                <BottomSheetView style={styles.contentContainer}>
-                    <Text>Awesome 🎉</Text>
-                </BottomSheetView>
-            </BottomSheet>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+        index={1} // initially closed
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        zIndex: 10,
-        padding: 24,
-        backgroundColor: 'grey',
-    },
-    contentContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',  // ensure content is centered
-    },
+  container: {
+    flex: 1,
+    zIndex: 10,
+    padding: 24,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center", // ensure content is centered
+  },
 });
